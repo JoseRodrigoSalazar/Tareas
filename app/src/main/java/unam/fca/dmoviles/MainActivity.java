e36ec325217d59ace6ab6891f1458f3a2b50de86
JoseRodrigoSalazar <joserodrigo_salazar@comunidad.unam.mx>
2019-03-17 01:20:02 -0600

    Actividad Realizada

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -2,8 +2,11 @@ package unam.fca.dmoviles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.time.LocalDateTime;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,14 +22,19 @@ public class MainActivity extends AppCompatActivity {
         // res/layout/activity_main.xml
         setContentView(R.layout.activity_main);
 
+       // final TextView tv = findViewById(R.id.tv);
 
         //Para hacer referencia a un componente del layout, utilizamos el metodo findViewById
         //y le pasamos como parametro el id correspondiente.
 
-        final TextView tv = findViewById(R.id.btn);
+        final TextView tv = findViewById(R.id.tv);
 
         final Button btn = findViewById(R.id.btn);
-
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                tv.setText(LocalDateTime.now().toString());
+            }
+        });
 
         //Para cambiar el texto de tv
         tv.setText("Hola mundo");
